5ef6a6f8e235a6af1d64f005ecdadc700368c97c
leon-lazuli <leonlazuli@LiuLeontekiMacBook-Air.local>
2013-11-07 00:36:50 -0800

    add the document  and the framework is done about 70%

---
+++ MomentsNotice/MomentsNotice/EventsViewController.h
@@ -0,0 +1,13 @@
+//
+//  EventsViewController.h
+//  MomentsNotice
+//
+//  Created by LiuLeon on 11/6/2013.
+//  Copyright (c) 2013 LiuLeon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventsViewController : UITableViewController
+
+@end
--- MomentsNotice/MomentsNotice/primaryMainMenuViewController.m
+++ MomentsNotice/MomentsNotice/primaryMainMenuViewController.m
@@ -15,20 +15,18 @@
 
 @implementation primaryMainMenuViewController
 
+-(void) prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    
+}
+
 //the setter for user, and we also set the welcome lable in this setter
 -(void) setUser:(MNUser *)user
 {
     _user = user;
 }
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
+
 
 - (void)viewDidLoad
 {
@@ -38,10 +36,6 @@
 	// Do any additional setup after loading the view.
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+
 
 @end
